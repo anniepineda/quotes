5c0b5dc9d813c5dff596b2fd160311a9292c4245
anniepineda <anniepineda@outlook.com>
2020-01-16 12:14:03 -0800

    generating quotes

---
+++ src/main/java/quotes/Quotes.java
@@ -0,0 +1,14 @@
+package quotes;
+
+public class Quotes {
+    String author;
+    String text;
+
+    @Override
+    public String toString() {
+        return "Quotes{" +
+                "author='" + author + '\'' +
+                ", text='" + text + '\'' +
+                '}';
+    }
+}
